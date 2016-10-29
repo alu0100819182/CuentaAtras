775c1543c2b960d3f07205f1924dc189d64bc823
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-29 15:37:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('paisajeUno').camera({ 
+	  height: '41%',
+	  pagination: false,
+	  thumbnails: true
+  });
+  $('paisajeDos').camera({ 
+	  height: '41%',
+	  pagination: false,
+	  thumbnails: true
+  });
+  $('paisajeTres').camera({ 
+	  height: '41%',
+	  pagination: false,
+	  thumbnails: true
+ });
+});
