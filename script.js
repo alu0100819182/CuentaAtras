0c1bf999cf685c9e2759bf2c41848250c6b24f35
Omar Adolfo Alvarez Hernandez <alu0100819182@ull.edu.es>
2016-10-29 16:39:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -55,4 +55,60 @@ $("#DateCountdowndos").TimeCircles({
         }
     }
 });
+$("#DateCountdowntres").TimeCircles({
+    "animation": "ticks",
+    "bg_width": 2.7,
+    "fg_width": 0.016666666666666666,
+    "circle_bg_color": "#60686F",
+    "time": {
+        "Days": {
+            "text": "Days",
+            "color": "#ff6675",
+            "show": true
+        },
+        "Hours": {
+            "text": "Hours",
+            "color": "#ffd099",
+            "show": true
+        },
+        "Minutes": {
+            "text": "Minutes",
+            "color": "#fdffbb",
+            "show": true
+        },
+        "Seconds": {
+            "text": "Seconds",
+            "color": "#afff99",
+            "show": true
+        }
+    }
+});
+$("#DateCountdowncuatro").TimeCircles({
+    "animation": "ticks",
+    "bg_width": 1.5,
+    "fg_width": 0.11,
+    "circle_bg_color": "#60686F",
+    "time": {
+        "Days": {
+            "text": "Days",
+            "color": "#66fcff",
+            "show": true
+        },
+        "Hours": {
+            "text": "Hours",
+            "color": "#ff99e1",
+            "show": true
+        },
+        "Minutes": {
+            "text": "Minutes",
+            "color": "#fff9bb",
+            "show": true
+        },
+        "Seconds": {
+            "text": "Seconds",
+            "color": "#999eff",
+            "show": true
+        }
+    }
+});
 });
